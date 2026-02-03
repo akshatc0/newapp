e9313a222a62b0c7803742cfaea7b36498b9ab6b
Akshat Chopra <akshat.chopra@uwaterloo.ca>
2026-02-03 17:28:27 -0500

    Fix Skiper88 sticky positioning and visibility

--- src/components/Skiper88.tsx
+++ src/components/Skiper88.tsx
@@ -25,40 +25,38 @@ const Skiper88 = () => {
   const rotateParent = useTransform(scrollYProgress, [0, 1], [-73, 242]);
 
   return (
-    <div ref={containerRef} className="relative h-[300vh] w-full overflow-hidden bg-[#eee]">
-      <div className="relative w-full">
-        <div
-          className="sticky top-1/2 -translate-y-1/2"
+    <div ref={containerRef} className="relative h-[300vh] w-full bg-[#eee]">
+      <div
+        className="sticky top-0 flex h-screen w-full items-center justify-center"
+        style={{
+          perspective: "500px",
+        }}
+      >
+        <motion.div
+          className="relative"
           style={{
-            perspective: "500px",
+            transformStyle: "preserve-3d",
+            rotateX: rotateParent,
           }}
         >
-          <motion.ul
-            className="relative w-full"
-            style={{
-              transformStyle: "preserve-3d",
-              rotateX: rotateParent,
-            }}
-          >
-            {items.map((item, index) => {
-              const angle = (-180 / items.length) * index;
+          {items.map((item, index) => {
+            const angle = (-180 / items.length) * index;
 
-              return (
-                <li
-                  key={index}
-                  className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 items-center justify-center text-4xl font-semibold tracking-tighter text-neutral-900 md:text-5xl"
-                  style={{
-                    transform: `rotateX(${angle}deg) translateZ(${radius}px)`,
-                    transformStyle: "preserve-3d",
-                    backfaceVisibility: "hidden",
-                  }}
-                >
-                  {item}
-                </li>
-              );
-            })}
-          </motion.ul>
-        </div>
+            return (
+              <div
+                key={index}
+                className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 items-center justify-center whitespace-nowrap text-4xl font-semibold tracking-tighter text-neutral-900 md:text-5xl"
+                style={{
+                  transform: `rotateX(${angle}deg) translateZ(${radius}px)`,
+                  transformStyle: "preserve-3d",
+                  backfaceVisibility: "hidden",
+                }}
+              >
+                {item}
+              </div>
+            );
+          })}
+        </motion.div>
       </div>
     </div>
   );
